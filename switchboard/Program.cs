be9eeae9e7aa87b7bc149174643f039dc2d82a59
Kohli, Amrit (AssetMark, Contractor) <Amrit.Kohli@AssetMark.com>
2019-03-06 12:53:45 -0800

    first pass at simplest model

---
+++ switchboard/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace switchboard
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var a = "i am a local a variable";
+
+			SwitchBoard.Switch("testswitch_a", () =>
+			{
+				Console.WriteLine($"testswitch_a is on! {a}");
+			}, () =>
+			{
+				Console.WriteLine("testswitch_a is off!");
+			}
+			);
+			Console.ReadLine();
+		}
+	}
+}
---
+++ switchboard/SwitchBoard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace switchboard
+{
+	public class SwitchBoard
+	{
+		private static Dictionary<string, bool> switches { get; set; }
+
+		static SwitchBoard()
+		{
+			switches = new Dictionary<string, bool>();
+
+			switches["testswitch_a"] = true;
+		}
+
+		public static void Switch(string switchName, Action onAction, Action offAction)
+		{
+			if(switches[switchName])
+			{
+				onAction();
+			} else
+			{
+				offAction();
+			}
+		}
+	}
+}
